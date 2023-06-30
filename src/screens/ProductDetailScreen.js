099263a8dc41b906aee382d6ce4e6888b00382fd
kavery-pm <shiny.kavery@gmail.com>
2023-06-30 12:40:56 +0200

    added the product detail screen and the linear gradient

--- src/screens/ProductDetailScreen.js
+++ src/screens/ProductDetailScreen.js
@@ -1,11 +1,29 @@
-import { View } from "react-native"
-
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Colors from '../../src/constants/colors'
 const ProductDetailScreen = ()=>{
     return (
+        <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+        style={styles.rootScreen}
+      >
         <View>
             <Text>Detail</Text>
         </View>
+        </LinearGradient>
     )
 
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent:'center',
+      textAlign:'center',
+  
+    },
+    rootScreen: {
+      flex: 1,
+    
+    },
+  });
 export default ProductDetailScreen
