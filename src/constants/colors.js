414f77f2801ed13f84a1c87d8b0b781c4e193881
kavery-pm <shiny.kavery@gmail.com>
2023-06-30 12:06:38 +0200

    added colors

---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+    primary500: '#ddb52f',
+    primary600: '#b3b52f',
+    primary700: '#8aaf30',
+    primary800: '#608f31',
+    accent500: '#007bff',   // Main blue color
+    blue100: '#b3d8ff',
+    blue200: '#80c1ff',
+    blue300: '#4daaff',
+    blue400: '#1a93ff',
+  };
+    
+  
+  export default Colors;
