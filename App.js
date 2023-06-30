3b480bfc0c4ddc9207ca99d56177af00b2164c0d
kavery-pm <shiny.kavery@gmail.com>
2023-06-30 12:21:18 +0200

    added react stack navigation

--- App.js
+++ App.js
@@ -3,14 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import Colors from './src/constants/colors'
 import ProductListScreen from './src/screens/ProductListScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
+    <>
+    <Stack.Navigator>
     <LinearGradient
     colors={[Colors.primary700, Colors.accent500]}
     style={styles.rootScreen}
   >
-  <ProductListScreen/>
+    <Stack.Screen name="Products" component={ProductListScreen} />
     </LinearGradient>
+  </Stack.Navigator>
+ 
+    </>
+   
   );
 }
 
---
+++ src/screens/ProductDetailScreen.js
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+
+const ProductDetailScreen = ()=>{
+    return (
+        <View>
+            <Text>Detail</Text>
+        </View>
+    )
+
+}
+export default ProductDetailScreen
