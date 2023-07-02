d78113176355cd1f4e8a1e1d13a2f1c3e4b60dc1
kavery-pm <shiny.kavery@gmail.com>
2023-07-02 10:17:40 +0200

    added typing animation for text

---
+++ src/animations/TypingAnimation.js
@@ -0,0 +1,25 @@
+import { useRef, useState, useEffect } from "react";
+import { Text } from "react-native";
+const TypewriterEffectText = ({ text, speed, style }) => {
+  const [animatedText, setAnimatedText] = useState("");
+  const textIndex = useRef(0);
+
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setAnimatedText((previousText) => {
+        if (textIndex.current < text.length) {
+          const nextCharacter = text[textIndex.current];
+          textIndex.current += 1;
+          return previousText + nextCharacter;
+        }
+        clearInterval(typingInterval); //cleanup
+        return previousText;
+      });
+    }, speed);
+
+    return () => clearInterval(typingInterval); // Clean up
+  }, [text, speed]);
+
+  return <Text style={style}>{animatedText}</Text>;
+};
+export default TypewriterEffectText;
