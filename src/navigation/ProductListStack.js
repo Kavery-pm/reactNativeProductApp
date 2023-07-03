01dbca19eebc545b8607ee6eb7c7b288b777972d
kavery-pm <shiny.kavery@gmail.com>
2023-07-03 15:14:39 +0200

    added stack navigation

---
+++ src/navigation/ProductListStack.js
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProductListScreen from '../screens/ProductListScreen';
+const Stack = createNativeStackNavigator();
+const ProductListStack = ()=>{
+    return (
+    <Stack.Navigator>
+     
+      <Stack.Screen name="Products" component={ProductListScreen} />
+    
+     
+    </Stack.Navigator>)
+}
+export default ProductListStack;
